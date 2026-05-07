82ee5b3780df17de2049bbb5d4b6768d024b2333
ACount5 <alvaroslkth@gmail.com>
2026-05-07 11:16:29 +0200

    Proyecto inicializado

---
+++ src/test/java/PokedexSpring/PokedexSpring/PokedexSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package PokedexSpring.PokedexSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokedexSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
